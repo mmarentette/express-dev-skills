e7786584b112ee72211d160f92f02677f4cd6d0c
mmarentette <melrosemarentette@gmail.com>
2023-10-18 00:44:43 -0400

    Add and export the show controller function

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,12 +1,20 @@
 const Skill = require('../models/skill');
 
 module.exports = {
-    index
+    index,
+    show
 }
 
 function index(req, res) {
     res.render('skills/index', {
         skills: Skill.getAll(),
         title: 'All Developer Skills'
+    });
+}
+
+function show(req, res) {
+    res.render('skills/show', {
+        skill: Skill.getOne(req.params.id),
+        title: "Skill Details"
     });
 }
