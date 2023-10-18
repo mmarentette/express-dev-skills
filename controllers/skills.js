1d1df3d28e21dc0a607dd12a467d863fb28abf79
mmarentette <melrosemarentette@gmail.com>
2023-10-18 00:47:26 -0400

    Add getOne method to skill model

--- controllers/skills.js
+++ controllers/skills.js
@@ -15,6 +15,6 @@ function index(req, res) {
 function show(req, res) {
     res.render('skills/show', {
         skill: Skill.getOne(req.params.id),
-        title: "Skill Details"
+        title: 'Skill Details'
     });
 }
